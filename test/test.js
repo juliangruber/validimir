92c22a44a22e7529c5a87f2377e185b7f9e021d7
Julian Gruber <julian@juliangruber.com>
2015-09-16 23:48:02 +0200

    update main test

--- test/test.js
+++ test/test.js
@@ -2,153 +2,155 @@ var test = require('tape');
 var v = require('..');
 
 test('number', function(t) {
-  v.number()(13);
-  t.throws(function() { v.number()('13') });
+  t.deepEqual(v().number()(13).errors, []);
+  t.deepEqual(v().number()('13').errors, [
+    { value: '13', operator: 'number', actual: 'string' }
+  ]);
   t.end();
 });
 
 test('string', function(t) {
-  v.string()('13');
-  t.throws(function() { v.string()(13) });
+  t.deepEqual(v().string()('13').errors, []);
+  t.deepEqual(v().string()(13).errors, [
+    { value: 13, operator: 'string', actual: 'number' }
+  ]);
   t.end();
 });
 
 test('boolean', function(t) {
-  v.boolean()(true);
-  t.throws(function() { v.boolean()() });
+  t.deepEqual(v().boolean()(true).errors, []);
+  t.deepEqual(v().boolean()('true').errors, [
+    { value: 'true', operator: 'boolean', actual: 'string' }  
+  ]);
   t.end();
 });
 
 test('object', function(t) {
-  v.object()({});
-  t.throws(function() { v.object()(null) });
-  t.throws(function() { v.object()('foo') });
+  t.deepEqual(v().object()({}).errors, []);
+  t.deepEqual(v().object()('true').errors, [
+    { value: 'true', operator: 'object', actual: 'string' }  
+  ]);
   t.end();
 });
 
+
 test('array', function(t) {
-  v.array()([]);
-  t.throws(function() { v.array()({}) });
+  t.deepEqual(v().array()([]).errors, []);
+  t.deepEqual(v().array()('true').errors, [
+    { value: 'true', operator: 'array', actual: 'string' }  
+  ]);
   t.end();
 });
 
 test('buffer', function(t) {
-  v.buffer()(new Buffer([]));
-  t.throws(function() { v.buffer()({}) });
+  t.deepEqual(v().buffer()(new Buffer(0)).errors, []);
+  t.deepEqual(v().buffer()({}).errors, [
+    { value: {}, operator: 'buffer', actual: 'object' }  
+  ]);
   t.end();
 });
 
+
 test('equal', function(t) {
-  v.equal('foo')('foo');
-  v.equal(4)(4);
+  t.deepEqual(v().equal('foo')('foo').errors, []);
+  t.deepEqual(v().equal(4)(4).errors, []);
 
-  v.equal({ gt: 4 })(6);
-  v.equal({ gt: 'a' })('b');
+  t.deepEqual(v().equal({ gt: 4 })(6).errors, []);
+  t.deepEqual(v().equal({ gt: 'a' })('b').errors, []);
 
-  v.equal({ lt: 7 })(6);
-  v.equal({ lt: 'b' })('a');
+  t.deepEqual(v().equal({ lt: 7 })(6).errors, []);
+  t.deepEqual(v().equal({ lt: 'b' })('a').errors, []);
 
-  var thrown = false;
-  try {
-    v.equal(1)(2);
-  } catch(e) {
-    thrown = true;
-    t.equal(e.message, 'not equal. expected: 1, is: 2');
-  } finally {
-    t.assert(thrown);
-  }
+  t.deepEqual(v().equal(1)(2).errors, [
+    { value: 2, operator: 'equal', expected: 1 }
+  ]);
 
-  t.throws(function() { v.equal('1')(1) });
-  t.throws(function() { v.equal({ gt: 4 })(3) });
-  t.throws(function() { v.equal({ gt: 'b' })('a') });
+
+  t.deepEquals(v().equal('1')(1).errors, [
+    { value: 1, operator: 'equal', expected: '1' }  
+  ]);
+  t.deepEquals(v().equal({ gt: 4 })(3).errors, [
+    { value: 3, operator: 'equal', expected: { gt: 4 } }  
+  ]);
+  t.deepEqual(v().equal({ gt: 'b' })('a').errors, [
+    { value: 'a', operator: 'equal', expected: { gt: 'b' } }
+  ]);
   t.end();
 });
 
 test('notEqual', function(t) {
-  v.notEqual('1')(1);
-  v.notEqual({ gt: 3, lt: 5 })(6);
-  t.throws(function() { v.notEqual('1')('1') });
+  t.deepEqual(v().notEqual('1')(1).errors, []);
+  t.deepEqual(v().notEqual({ gt: 3, lt: 5 })(6).errors, []);
+  t.deepEqual(v().notEqual('1')('1').errors, [
+    { value: '1', operator: 'notEqual' }
+  ]);
   t.end();
 });
 
 test('match', function(t) {
-  v.match(/foo/)('foo');
-
-  var threw = false;
-  try {
-    v.match(/foo/)('f');
-  } catch(err) {
-    threw = true;
-    t.equal(err.message, 'doesn\'t match (is f)');
-  }
-
-  t.ok(threw);
+  t.deepEqual(v().match(/foo/)('foo').errors, []);
+  t.deepEqual(v().match(/foo/)('f').errors, [
+    { value: 'f', operator: 'match', expected: /foo/ }
+  ]);
   t.end();
 });
 
 test('notMatch', function(t) {
-  v.notMatch(/foo/)('f');
-
-  var threw = false;
-  try {
-    v.notMatch(/foo/)('foo');
-  } catch(err) {
-    threw = true;
-    t.equal(err.message, 'does match (is foo)');
-  }
-
-  t.ok(threw);
+  t.deepEqual(v().notMatch(/foo/)('f').errors, []);
+  t.deepEqual(v().notMatch(/foo/)('foo').errors, [
+    { value: 'foo', operator: 'notMatch' }
+  ]);
   t.end();
 });
 
 test('hasKey', function(t) {
-  v.hasKey('a')({a:'b'});
-  t.throws(function() { v.hasKey('b')({a:'b'}) });
+  t.deepEqual(v().hasKey('a')({a:'b'}).errors, []);
+  t.deepEqual(v().hasKey('b')({a:'b'}).errors, [
+    { value: { a: 'b' }, operator: 'hasKey', expected: 'b' }
+  ]);
   t.end();
 });
 
 test('len', function(t) {
-  t.plan(3);
+  t.deepEqual(v().len(13)('aaaaaaaaaaaaa').errors, []);
+  t.deepEqual(v().len({ gt: 3 })('aaaaa').errors, []);
+  t.deepEqual(v().len({ lte: 10 })('aaaaa').errors, []);
+  t.deepEqual(v().len({ gt: 3, lte: 10 })('aaaaa').errors, []);
 
-  v.len(13)('aaaaaaaaaaaaa');
-  v.len({ gt: 3 })('aaaaa');
-  v.len({ lte: 10 })('aaaaa');
-  v.len({ gt: 3, lte: 10 })('aaaaa');
+  t.deepEqual(v().len(13)('a').errors, [
+    { value: 'a', operator: 'len', expected: 13, actual: 1 }
+  ]);
+  t.deepEqual(v().len({ gt: 3 })('a').errors, [
+    { value: 'a', operator: 'len', expected: { gt: 3 }, actual: 1 }
+  ]);
+  t.deepEqual(v().len({ lte: 3 })('aaaaa').errors, [
+    { value: 'aaaaa', operator: 'len', expected: { lte: 3 }, actual: 5 }
+  ]);
+  t.deepEqual(v().len({ gt: 3, lte: 10 })('a').errors, [
+    { value: 'a', operator: 'len', expected: { gt: 3, lte: 10 }, actual: 1 }
+  ]);
 
-  t.throws(function() {
-    v.len(13)('a');
-    v.len({ gt: 3 })('a');
-    v.len({ lte: 3 })('aaaaa');
-    v.len({ gt: 3, lte: 10 })('a');
-  });
-
-  try {
-    var fn = v.rules({
-      'title': v.len({ gte: 3 })
-    });
-    fn({ title: 'a' });
-  } catch(err) {
-    t.ok(err);
-    t.equal(err.message, 'title length not in range {\"gte\":3}');
-  }
+  t.end();
 });
 
 test('of', function(t) {
-  v.of(['foo', 'bar'])('foo');
-  t.throws(function() { v.of(['foo'])('bar') });
+  t.deepEqual(v().of(['foo', 'bar'])('foo').errors, []);
+  t.deepEqual(v().of(['foo'])('bar').errors, [
+    { value: 'bar', operator: 'of', expected: ['foo'] }
+  ]);
   t.end();
 });
 
 test('notOf', function(t) {
-  v.notOf(['foo', 'bar'])('baz');
-  t.throws(function() { v.notOf(['foo'])('foo') });
+  t.deepEqual(v().notOf(['foo', 'bar'])('baz').errors, []);
+  t.deepEqual(v().notOf(['foo'])('foo').errors, [
+    { value: 'foo', operator: 'notOf', expected: ['foo'] }
+  ]);
   t.end();
 });
 
 test('integration', function(t) {
-  v.of(['foo']).len(3).match(/^foo$/)
-  .notEqual('bar').equal('foo').string()('foo');
-
-  t.ok(true);
+  t.deepEqual(v().of(['foo']).len(3).match(/^foo$/)
+  .notEqual('bar').equal('foo').string()('foo').errors, []);
   t.end();
 });
