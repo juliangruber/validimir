0791e816b1016abe8f58a27a70abe13d1dc0db54
Julian Gruber <julian@juliangruber.com>
2015-09-16 23:47:41 +0200

    remove old tests

--- test/optional.js
+++
@@ -1,9 +0,0 @@
-var v = require('..');
-var test = require('tape');
-
-test('optional', function(t) {
-  v.optional().string()();
-  v.string().optional()();
-  t.throws(function() { v.string()() });
-  t.end();
-});
