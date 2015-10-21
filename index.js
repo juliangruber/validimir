bbb6944eabb3acb34737e70a353277281f720d9d
Julian Gruber <julian@juliangruber.com>
2015-10-21 12:07:08 +0200

    refactor

--- index.js
+++ index.js
@@ -20,174 +20,172 @@ module.exports = function V(){
       }
     };
   };
+  var addCheck = function(op, fn){
+    v[op] = function(){
+      var check = fn.apply(null, arguments);
+      checks.push(check);
+      return v;
+    };
+  };
 
   var types = 'number string boolean object array buffer date'.split(' ');
   types.forEach(function(t){
-    v[t] = function(msg){
-      checks.push(function(v){
+    addCheck(t, function(msg){
+      return function(v){
         if (type(v) != t) return {
           value: v,
           operator: t,
           actual: type(v),
           message: msg || fmt('Expected a %s but got a %s', t, type(v))
         };
-      });
-      return v;
-    };
+      };
+    });
   });
 
-  v.email = function(msg){
-    checks.push(function(e){
-      if (!/^([\w_\.\-\+])+\@([\w\-]+\.)+([\w]{2,10})+$/.test(e)) return {
+  addCheck('email', function(msg){
+    return function(e){
+      var match = /^([\w_\.\-\+])+\@([\w\-]+\.)+([\w]{2,10})+$/.test(e);
+      if (!match) return {
         value: e,
         operator: 'email',
         message: msg || 'Expected a valid email address'
       };
-    });
-    return v;
-  };
+    };
+  });
 
-  v.equal = function(expected, msg){
+  addCheck('equal', function(expected, msg){
     if (typeof expected == 'object') {
-      checks.push(function(v){
+      return function(v){
         if (!ltgt.contains(expected, v)) return {
           value: v,
           operator: 'equal',
           expected: expected,
           message: msg || fmt('Expected a value in range %s', toInterval(expected))
         };
-      });
+      };
     } else {
-      checks.push(function(v){
+      return function(v){
         if (v !== expected) return {
           value: v,
           operator: 'equal',
           expected: expected,
           message: msg || fmt('Expected %j to equal %j', v, expected)
         };
-      });
+      };
     }
-    return v;
-  };
+    
+  });
 
-  v.notEqual = function(notExpected, msg){
+  addCheck('notEqual', function(notExpected, msg){
     if (typeof notExpected == 'object') {
-      checks.push(function(v){
+      return function(v){
         if (ltgt.contains(notExpected, v)) return {
           value: v,
           operator: 'notEqual',
           message: msg
             || fmt('Expected a value outside range %s', toInterval(notExpected))
         };
-      });
+      };
     } else {
-      checks.push(function(v){
+      return function(v){
         if (v === notExpected) return {
           value: v,
           operator: 'notEqual',
           message: msg || fmt('Expected %j not to equal %j', v, notExpected)
         };
-      });
+      };
     }
-    return v;
-  };
+  });
 
-  v.match = function(reg, msg){
-    checks.push(function(v){
+  addCheck('match', function(reg, msg){
+    return function(v){
       if (!reg.test(v)) return {
         value: v,
         operator: 'match',
         expected: reg,
         message: msg || fmt('Expected %j to match %s', v, reg)
       };
-    });
-    return v;
-  };
+    };
+  });
 
-  v.notMatch = function(reg, msg){
-    checks.push(function(v){
+  addCheck('notMatch', function(reg, msg){
+    return function(v){
       if (reg.test(v)) return {
         value: v,
         operator: 'notMatch',
         message: msg || fmt('Expected %j not to match %s', v, reg)
       };
-    });
-    return v;
-  };
+    };
+  });
 
-  v.hasKey = function(k, msg){
-    checks.push(function(o){
+  addCheck('hasKey', function(k, msg){
+    return function(o){
       if (type(o) != 'object' || !(k in o)) return {
         value: o,
         operator: 'hasKey',
         expected: k,
         message: msg || fmt('Expected %j to have key %s', o, k)
       };
-    });
-    return v;
-  };
+    };
+  });
 
-  v.len = function(l, msg){
+  addCheck('len', function(l, msg){
     if (typeof l == 'number') {
-      checks.push(function(s){
-        if (s.length != l) return {
+      return function(s){
+        if (getLength(s) != l) return {
           value: s,
           operator: 'len',
           expected: l,
-          actual: s.length,
+          actual: getLength(s),
           message: msg || fmt('Expected %j to have length %s', s, l)
         };
-      });
+      };
     } else {
-      checks.push(function(s){
-        if (!ltgt.contains(l, s.length)) return {
+      return function(s){
+        if (!ltgt.contains(l, getLength(s))) return {
           value: s,
           operator: 'len',
           expected: l,
-          actual: s.length,
+          actual: getLength(s),
           message: msg
             || fmt('Expected %j to be of length %s', s, toInterval(l))
         };
-      });
+      };
     }
-    return v;
-  };
+  });
 
-  v.of = function(arr, msg){
-    checks.push(function(v){
+  addCheck('of', function(arr, msg){
+    return function(v){
       if (arr.indexOf(v) == -1) return {
         value: v,
         operator: 'of',
         expected: arr,
         message: msg || fmt('Expected %j to be of %j', v, arr)
       };
-    });
-    return v;
-  };
+    };
+  });
 
-  v.notOf = function(arr, msg){
-    checks.push(function(v){
+  addCheck('notOf', function(arr, msg){
+    return function(v){
       if (arr.indexOf(v) > -1) return {
         value: v,
         operator: 'notOf',
         expected: arr,
         message: msg || fmt('Expected %j not to be of %j', v, arr)
       };
-    });
-    return v;
-  };
+    };
+  });
 
-  v.each = function(fn){
-    checks.push(function(o){
+  addCheck('each', function(fn){
+    return function(o){
       var errors = [];
       Object.keys(o).forEach(function(key){
         var errs = fn(o[key]).errors;
         errs.forEach(function(err) { errors.push(err) });
       });
       if (errors.length) return errors;
-    });
-    return v;
-  };
+    };
+  });
 
   return v;
 };
@@ -195,4 +193,8 @@ module.exports = function V(){
 module.exports.putin = function(key, value, cb) {
   cb(new Error('in node, error throws you!'));
 };
+
+function getLength(obj){
+  return obj.length;
+}
 
